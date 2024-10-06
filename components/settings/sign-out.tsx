3b53533e5412e98ad7ec7bf1d52e0e9f9dac65dc
CodeNoJutsu <bellokhalid74@gmail.com>
2024-10-06 14:47:15 +0100

    Just test

--- components/settings/sign-out.tsx
+++ components/settings/sign-out.tsx
@@ -1,28 +1,31 @@
-'use client';
+"use client";
 
 import { faSignOut } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRouter } from 'next/navigation';
-import showClicked from '@/app/utils/clicked';
+import { useRouter } from "next/navigation";
+import showClicked from "@/app/utils/clicked";
 import React, { useRef } from "react";
 
 export default function SignOut() {
-    const router = useRouter();
-    const signOutBtRef = useRef<null | HTMLButtonElement>(null);
+  const router = useRouter();
+  const signOutBtRef = useRef<null | HTMLButtonElement>(null);
 
-    const signOut = (e: React.MouseEvent) => {
-        if (signOutBtRef.current) showClicked(signOutBtRef.current);
-        setTimeout(() => router.push('/sign-in'), 250);
-    }
+  const signOut = (e: React.MouseEvent) => {
+    if (signOutBtRef.current) showClicked(signOutBtRef.current);
+    setTimeout(() => router.push("/sign-in"), 250);
+  };
 
-    return (
-        <button
-            ref={signOutBtRef}
-            onClick={signOut}
-            className="mx-auto flex justify-center items-center gap-4 mt-14"
-        >
-            <FontAwesomeIcon icon={faSignOut} className="bg-blue-50 text-blue-600 w-5 h-5 p-2 rounded-xl" />
-            SignOut
-        </button>
-    )
+  return (
+    <button
+      ref={signOutBtRef}
+      onClick={signOut}
+      className="mx-auto flex justify-center items-center gap-4 mt-14"
+    >
+      <FontAwesomeIcon
+        icon={faSignOut}
+        className="bg-blue-50 text-blue-600 w-5 h-5 p-2 rounded-xl"
+      />
+      SignOut
+    </button>
+  );
 }
