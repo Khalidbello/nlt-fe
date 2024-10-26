a5b113948e70495c3281af3a0f37d781e7a0dcff
CodeNoJutsu <bellokhalid74@gmail.com>
2024-10-26 23:28:04 +0100

    Modified course view image to fetch individually

--- app/course-view/page.tsx
+++ app/course-view/page.tsx
@@ -1,203 +1,223 @@
-'use client';
+"use client";
 
-import Header from '@/components/multipurpose/header';
-import Name from '@/components/course-view/name';
-import About from '@/components/course-view/about';
-import FloatingEnrollButton from '@/components/course-view/enroll-bt';
-import Chapter, { chapterType } from '@/components/course-view/chapter';
-import { Suspense, useEffect, useState } from 'react';
-import Loader from '@/components/multipurpose/loader';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
-import DoughnutChart from '@/components/course-view/doughnu';
-import ContinueLearningBT from '@/components/course-view/continue-bt';
-import { useRouter, useSearchParams } from 'next/navigation';
-import EnterReview from '@/components/course-view/enter-review';
-import HomeBt from '@/components/multipurpose/home-bt';
-
+import Header from "@/components/multipurpose/header";
+import Name from "@/components/course-view/name";
+import About from "@/components/course-view/about";
+import FloatingEnrollButton from "@/components/course-view/enroll-bt";
+import Chapter, { chapterType } from "@/components/course-view/chapter";
+import { Suspense, useEffect, useState } from "react";
+import Loader from "@/components/multipurpose/loader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import DoughnutChart from "@/components/course-view/doughnu";
+import ContinueLearningBT from "@/components/course-view/continue-bt";
+import { useRouter, useSearchParams } from "next/navigation";
+import EnterReview from "@/components/course-view/enter-review";
+import HomeBt from "@/components/multipurpose/home-bt";
 
 interface courseDataType {
-    courseName: string;
-    courseId: number;
-    about: string;
-    enrolled: boolean;
-    quizPerfomace: number;
-    progress: number;
-    chapters: chapterType[];
-    currentChapter: number;
-    currentChapterId: number;
-    currentLesson: number;
-    image: string;
-    lessonNumbers: { [key: number]: number };
-    completed: boolean;
-    reviewed: boolean;
+  courseName: string;
+  courseId: number;
+  about: string;
+  enrolled: boolean;
+  quizPerfomace: number;
+  progress: number;
+  chapters: chapterType[];
+  currentChapter: number;
+  currentChapterId: number;
+  currentLesson: number;
+  image: string;
+  lessonNumbers: { [key: number]: number };
+  completed: boolean;
+  reviewed: boolean;
 }
 
 const CourseView = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const [courseData, setCourseData] = useState<courseDataType>({
-        courseName: '',
-        courseId: 0,
-        about: '',
-        enrolled: false,
-        quizPerfomace: 0,
-        progress: 0,
-        chapters: [],
-        currentChapter: 0,
-        currentChapterId: 0,
-        currentLesson: 0,
-        lessonNumbers: { 0: 0 },
-        image: '',
-        completed: false,
-        reviewed: false,
-    });
-    const [showLoader, setShowLoader] = useState<boolean>(true);
-    const [showError, setShowError] = useState<boolean>(false);
-    const [reload, setReload] = useState<boolean>(false);
-    const apiHost = process.env.NEXT_PUBLIC_API_HOST;
-    const courseId = searchParams.get('course_id');
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [courseData, setCourseData] = useState<courseDataType>({
+    courseName: "",
+    courseId: 0,
+    about: "",
+    enrolled: false,
+    quizPerfomace: 0,
+    progress: 0,
+    chapters: [],
+    currentChapter: 0,
+    currentChapterId: 0,
+    currentLesson: 0,
+    lessonNumbers: { 0: 0 },
+    image: "",
+    completed: false,
+    reviewed: false,
+  });
+  const [showLoader, setShowLoader] = useState<boolean>(true);
+  const [showError, setShowError] = useState<boolean>(false);
+  const [reload, setReload] = useState<boolean>(false);
+  const apiHost = process.env.NEXT_PUBLIC_API_HOST;
+  const courseId = searchParams.get("course_id");
 
-    const fetchData = async () => {
-        try {
-            setShowLoader(true);
-            setShowError(false);
+  const fetchData = async () => {
+    try {
+      setShowLoader(true);
+      setShowError(false);
 
-            const response = await fetch(`${apiHost}/users/course-view/${courseId}`, {
-                method: 'GET',
-                credentials: 'include',
-            });
+      const response = await fetch(`${apiHost}/users/course-view/${courseId}`, {
+        method: "GET",
+        credentials: "include",
+      });
 
-            if ((await response).status === 200) {
-                const data = await response.json();
-                setCourseData(data)
-            } else if (response.status === 403) {
-                router.push('/sign-in?redirect=true');
-            } else {
-                throw 'somrthng went wrong';
-            }
-        } catch (err) {
-            console.log('error in course view', err);
-            setShowError(true);
-        } finally {
-            setShowLoader(false);
-        };
-    };
+      if ((await response).status === 200) {
+        const data = await response.json();
+        setCourseData(data);
+      } else if (response.status === 403) {
+        router.push("/sign-in?redirect=true");
+      } else {
+        throw "somrthng went wrong";
+      }
+    } catch (err) {
+      console.log("error in course view", err);
+      setShowError(true);
+    } finally {
+      setShowLoader(false);
+    }
+  };
 
-    useEffect(() => {
-        fetchData();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [reload]);
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reload]);
 
-    return (
-        <div className="w-full h-full pt-16">
-            <Header heading='Course view' />
-            {showLoader ? (
-                <div className="flex justify-center items-center h-[90%]">
-                    <Loader h='h-[7rem]' />
-                </div>
-            ) : (
-                <div className="">
-                    {showError ? (
-                        <div className="error-container mx-4 bg-red-100 text-red-500 p-4 rounded-lg shadow-md mt-8">
-                            <FontAwesomeIcon icon={faExclamationTriangle} className="mr-2" />
-                            <span className="text-lg">Something went wrong</span>
-                            <button onClick={() => setReload(!reload)} className="bg-white text-red-500 px-4 py-2 ml-4 rounded-md shadow-md">
-                                Reload
-                            </button>
-                        </div>
-                    ) : (
-                        // @ts-ignore
-                        <Main courseData={courseData} courseId={parseInt(courseId)} />
-                    )}
-                </div>
-            )}
+  return (
+    <div className="w-full h-full pt-16">
+      <Header heading="Course view" />
+      {showLoader ? (
+        <div className="flex justify-center items-center h-[90%]">
+          <Loader h="h-[7rem]" />
         </div>
-    );
+      ) : (
+        <div className="">
+          {showError ? (
+            <div className="error-container mx-4 bg-red-100 text-red-500 p-4 rounded-lg shadow-md mt-8">
+              <FontAwesomeIcon icon={faExclamationTriangle} className="mr-2" />
+              <span className="text-lg">Something went wrong</span>
+              <button
+                onClick={() => setReload(!reload)}
+                className="bg-white text-red-500 px-4 py-2 ml-4 rounded-md shadow-md"
+              >
+                Reload
+              </button>
+            </div>
+          ) : (
+            // @ts-ignore
+            <Main courseData={courseData} courseId={parseInt(courseId)} />
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
-const Main: React.FC<{ courseData: courseDataType; courseId: number }> = ({ courseData, courseId }) => {
-    const [showEnterReview, setShowEnterReview] = useState<boolean>(false);
+const Main: React.FC<{ courseData: courseDataType; courseId: number }> = ({
+  courseData,
+  courseId,
+}) => {
+  const [showEnterReview, setShowEnterReview] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (courseData.currentChapter > 1 && !courseData.reviewed) setShowEnterReview(true);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-    const data = {
-        labels: ['Failed', 'Passed',],
-        datasets: [
-            {
-                data: [100 - courseData.quizPerfomace, courseData.quizPerfomace],
-                backgroundColor: ['#f56565', '#4299e1'],
-            },
-        ],
-    };
+  useEffect(() => {
+    if (courseData.currentChapter > 1 && !courseData.reviewed)
+      setShowEnterReview(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const data = {
+    labels: ["Failed", "Passed"],
+    datasets: [
+      {
+        data: [100 - courseData.quizPerfomace, courseData.quizPerfomace],
+        backgroundColor: ["#f56565", "#4299e1"],
+      },
+    ],
+  };
 
-    return (
+  return (
+    <>
+      <HomeBt />
+      <Name name={courseData.courseName} courseId={courseData.courseId} />
+      {courseData.enrolled && (
         <>
-            <HomeBt />
-            <Name name={courseData.courseName} image={courseData.image} />
-            {courseData.enrolled && (
-                <>
-                    <div className='mt-2'>
-                        <h3 className='px-4 font-medium'>Quiz Performance</h3>
-                        <DoughnutChart percentage={courseData.progress} data={data} />
-                    </div>
-                    <div className="flex items-center mb-6 mx-4">
-                        <p className="text-gray-600 mr-2">Progress:</p>
-                        <div className="w-full h-2 rounded-full bg-gray-100">
-                            <div
-                                className="h-full rounded-full bg-blue-500"
-                                style={{ width: `${courseData.completed ? 100 : courseData.progress.toFixed(2)}%` }}
-                            />
-                        </div>
-                        <p className="text-gray-600 ml-2 font-medium">{courseData.completed ? 100 : courseData.progress.toFixed(2)}%</p>
-                    </div>
-                </>
-            )}
-
-            <About text={courseData.about} limit={200} />
-
-            {courseData.chapters.map((chapter: chapterType, index: number) => {
-                return (
-                    <Chapter
-                        key={index}
-                        chapter={chapter}
-                        courseId={courseId}
-                        lessonNumber={courseData.lessonNumbers[chapter.chapter_number]}
-                        currentChapter={courseData.currentChapter}
-                        currentLesson={courseData.currentLesson}
-                    />
-                )
-            })}
-
-            {courseData.enrolled ? (
-                courseData.completed ? (
-                    <p className='fixed bottom-0 left-0 w-full bg-blue-500 text-white py-2 text-center'>Completed</p>
-                ) : (<ContinueLearningBT
-                    courseId={courseId}
-                    chapterId={courseData.currentChapterId}
-                    chapterNUmber={courseData.currentChapter}
-                    lessonNumber={courseData.currentLesson}
-                />)
-            ) : (
-                <FloatingEnrollButton courseId={courseId} />
-            )}
-
-            {showEnterReview && <EnterReview courseName={courseData.courseName} courseId={courseData.courseId} hide={setShowEnterReview} />}
-            <div className='h-16'></div>
+          <div className="mt-2">
+            <h3 className="px-4 font-medium">Quiz Performance</h3>
+            <DoughnutChart percentage={courseData.progress} data={data} />
+          </div>
+          <div className="flex items-center mb-6 mx-4">
+            <p className="text-gray-600 mr-2">Progress:</p>
+            <div className="w-full h-2 rounded-full bg-gray-100">
+              <div
+                className="h-full rounded-full bg-blue-500"
+                style={{
+                  width: `${
+                    courseData.completed ? 100 : courseData.progress.toFixed(2)
+                  }%`,
+                }}
+              />
+            </div>
+            <p className="text-gray-600 ml-2 font-medium">
+              {courseData.completed ? 100 : courseData.progress.toFixed(2)}%
+            </p>
+          </div>
         </>
-    );
+      )}
+
+      <About text={courseData.about} limit={200} />
+
+      {courseData.chapters.map((chapter: chapterType, index: number) => {
+        return (
+          <Chapter
+            key={index}
+            chapter={chapter}
+            courseId={courseId}
+            lessonNumber={courseData.lessonNumbers[chapter.chapter_number]}
+            currentChapter={courseData.currentChapter}
+            currentLesson={courseData.currentLesson}
+          />
+        );
+      })}
+
+      {courseData.enrolled ? (
+        courseData.completed ? (
+          <p className="fixed bottom-0 left-0 w-full bg-blue-500 text-white py-2 text-center">
+            Completed
+          </p>
+        ) : (
+          <ContinueLearningBT
+            courseId={courseId}
+            chapterId={courseData.currentChapterId}
+            chapterNUmber={courseData.currentChapter}
+            lessonNumber={courseData.currentLesson}
+          />
+        )
+      ) : (
+        <FloatingEnrollButton courseId={courseId} />
+      )}
+
+      {showEnterReview && (
+        <EnterReview
+          courseName={courseData.courseName}
+          courseId={courseData.courseId}
+          hide={setShowEnterReview}
+        />
+      )}
+      <div className="h-16"></div>
+    </>
+  );
 };
-
-
 
 const Page: React.FC = () => {
-    return (
-        <Suspense fallback={<Loader h={'h-[5rem]'} />}>
-            <CourseView />
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<Loader h={"h-[5rem]"} />}>
+      <CourseView />
+    </Suspense>
+  );
 };
 
 export default Page;
--- components/course-view/name.tsx
+++ components/course-view/name.tsx
@@ -1,24 +1,63 @@
+"use client";
+
+import { faImage } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 interface NameProps {
   name: string;
-  image: string;
+  courseId: number;
 }
 
-const Name: React.FC<NameProps> = ({ name, image }) => {
-  console.log("image", image);
+const Name: React.FC<NameProps> = ({ name, courseId }) => {
+  const [image, setImage] = useState<null | string>(null);
+  const apiHost = process.env.NEXT_PUBLIC_API_HOST;
+
+  const fetchImage = async () => {
+    try {
+      console.log("Course id for course vewwwwwwww", courseId);
+      const response = await fetch(
+        `${apiHost}/users/course-image/${courseId}`,
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+
+      if (response.status == 200) {
+        const data = await response.json();
+        setImage(data.image);
+      }
+    } catch (err) {
+      console.error("An error occured fetching image for course", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchImage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="mb-4 px-4">
       <h2 className="mb-2 font-semibold text-xl">{name}</h2>
       <div className="mx-2 bg-blue-200 p-1 rounded-lg">
-        <Image
-          alt="course image"
-          src={`data:image/jpeg;base64,${image}`}
-          objectFit="cover" // To maintain the image's aspect ratio
-          height={500}
-          width={500}
-          className="w-full h-full rounded-lg"
-        />
+        {image ? (
+          <Image
+            alt="course image"
+            src={`data:image/jpeg;base64,${image}`}
+            objectFit="cover" // To maintain the image's aspect ratio
+            height={500}
+            width={500}
+            className="w-full h-full rounded-lg"
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faImage}
+            className="w-full min-h-[17rem] text-gray-300"
+          />
+        )}
       </div>
     </div>
   );
